5e780e2c07bb00cff9588ca6bd2a92d8026d58ad
Sakthiprasath1 <144891345+Sakthiprasath1@users.noreply.github.com>
2023-09-13 13:50:45 +0530

    Add files via upload

---
+++ leap.py
@@ -0,0 +1,14 @@
+# Leap year program in Python using if else
+
+year = int(input("Enter a year: "))
+
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print(year, "is a leap year")
+        else:
+            print(year, "is not a leap year")
+    else:
+        print(year, "is a leap year")
+else:
+    print(year, "is not a leap year")
